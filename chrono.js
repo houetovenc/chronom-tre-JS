501decc885f137fe44e135206f445e9eb3a8bc6d
houeto venceslas <79464349+houetovenc@users.noreply.github.com>
2022-11-27 22:12:58 +0100

    ajout des centième

--- chrono.js
+++ chrono.js
@@ -1,20 +1,26 @@
 var h2 = document.getElementsByTagName('h2')[0];
+var h3 = document.getElementsByTagName('h3')[0];
 var start = document.getElementById('strt');
 var stop = document.getElementById('stp');
 var reset = document.getElementById('rst');
 var sec = 0;
 var min = 0;
 var hrs = 0;
+var mil = 00;
 var t;
 
 function tick(){
-    sec++;
-    if (sec >= 60) {
-        sec = 0;
-        min++;
-        if (min >= 60) {
-            min = 0;
-            hrs++;
+    mil++;
+    if (mil >= 100) {
+        mil = 0;
+        sec++;
+        if (sec >= 60) {
+            sec = 0;
+            min++;
+            if (min >= 60) {
+                min = 0;
+                hrs++;
+            }
         }
     }
 }
@@ -23,10 +29,13 @@ function add() {
     h2.textContent = (hrs > 9 ? hrs : "0" + hrs) 
         	 + ":" + (min > 9 ? min : "0" + min)
        		 + ":" + (sec > 9 ? sec : "0" + sec);
+    h3.textContent =
+            (mil > 99 ? mil : "0" + mil);
+
     timer();
 }
 function timer() {
-    t = setTimeout(add, 1000);
+    t = setTimeout(add, 10);
 }
 
 timer();
@@ -36,5 +45,6 @@ stop.onclick = function() {
 }
 reset.onclick = function() {
     h2.textContent = "00:00:00";
-    seconds = 0; minutes = 0; hours = 0;
+    h3.textContent = "00";
+    sec = 0; min = 0; hrs = 0; mil = 0;
 }
